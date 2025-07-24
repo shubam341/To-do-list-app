3b7e9b2be4c1aef695627d6fe610f800a4e42908
shubam <chshivam148@gmail.com>
2025-07-25 00:28:55 +0530

    Added layout and  todo item section

--- src/components/PrivateRoute.tsx
+++
@@ -1,8 +0,0 @@
-// import { Navigate } from 'react-router-dom';
-// import { useAuth } from '../contexts/AuthContext';
-
-// export default function PrivateRoute({ children }: { children: JSX.Element }) {
-//   const { isAuthenticated } = useAuth();
-
-//   return isAuthenticated ? children : <Navigate to="/login" />;
-// }
